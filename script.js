8b4742ac5fc3c8b67bdb5caa485bb108995c77f3
Sung <sung272012@hotmail.com>
2024-07-16 16:58:56 -0400

    Changes main

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let firstNumber = '';
 let operator = '';
 let secondNumber = '';
-
+let operatorToggle = false;
+let displayValue = '0';
 
 
 const add = function(a, b) {
@@ -17,37 +18,70 @@ const multiply = function(a, b) {
 }
 
 const divide = function(a, b) {
+    if (b === 0) {
+        return 'NOPE';
+    }
     return a / b;
 }
 
 const operate = function(operator, firstNum, secondNum) {
     if (operator === '+') {
-        return add(firstNum, secondNum);
+        return add(+firstNum, +secondNum);
     } else if (operator === '-') {
-        return subtract(firstNum, secondNum);
+        return subtract(+firstNum, +secondNum);
     } else if (operator === '*') {
-        return multiply(firstNum, secondNum);
+        return multiply(+firstNum, +secondNum);
     } else {
-        return divide(firstNum, secondNum);
+        return divide(+firstNum, +secondNum);
     }
 }
 
+const numButtons = Array.from(document.querySelectorAll(".number"));
+const operators = Array.from(document.querySelectorAll(".operator"));
+const dotButton = document.querySelector(".dot");
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector(".clear");
+const display = document.querySelector(".display");
+
+
+
 const populateDisplay = function() {
-    let display = document.querySelector(".display");
-    let numButtons = document.querySelectorAll(".number");
+    
     for (const btn of numButtons) {
         btn.addEventListener("click", () => {
             numberClicked(btn, display);
         });
     }
-    let clearButton = document.querySelector(".clear");
+    
     clearButton.addEventListener("click", () => {
         clear();
-        display.textContent = 0});
+        display.textContent = 0
+    });
+    
+    dotButton.addEventListener("click", () => {
+        if (!display.textContent.includes('.')) 
+            display.textContent += '.';
+    });
+
+    for (const operatorButton of operators) {
+        operatorButton.addEventListener("click", () => {
+            operatorClicked(operatorButton);
+        })
+    }
+
+    equalButton.addEventListener("click", ()=>{
+        display.textContent = operate(operator, firstNumber, secondNumber);
+    })
 }
 
 const numberClicked = function(btn, display) {
-    if (display.textContent === '0') {
+    if (operatorToggle && secondNumber === '') {
+        display.textContent = btn.textContent;
+        secondNumber += btn.textContent;
+    } else if (operatorToggle && secondNumber !== '') {
+        display.textContent += btn.textContent;
+        secondNumber += btn.textContent;
+    } else if (display.textContent === '0') {
         display.textContent = btn.textContent;
     } else if (display.textContent.length < 10) {
         display.textContent += btn.textContent;
@@ -58,6 +92,20 @@ const clear = function() {
     firstNumber = '';
     secondNumber = '';
     operator = '';
+}
+
+const operatorClicked = function(operatorButton) {
+    operatorButton.classList.toggle("operatorClick");
+    operatorToggle = !operatorToggle;
+    firstNumber = display.textContent;
+    operator = operatorButton.textContent;
+}
+
+const equalClicked = function() {
+    const result = operate(operator, firstNumber, secondNumber);
+    firstNumber = result;
+    operatorToggle = !operatorToggle;
+
 }
 
 populateDisplay();
